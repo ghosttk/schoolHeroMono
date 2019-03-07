354adcd5aa9d0718b7e507b44cd32e22d97fad07
ghosttk <ghosttk@hotmail.com>
2019-03-07 10:25:47 +0800

    add drag action

--- main.cs
+++ main.cs
@@ -6,15 +6,16 @@ using System.IO;
 namespace schoolHero {
 	public class mainForm: Form {
 		 private Point _frmPoint;
+		 ContextMenu context_menu;
 			private mainForm()
 			{
-				 //无边框
             	this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            //不出现在任务栏
-            	this.ShowInTaskbar = false; 
-				this.TopMost = true;
+            	this.TopMost = true;
 				this.MouseDown += MainFrm_MouseDown;
           		this.MouseMove += MainFrm_MouseMove;
+				this.MouseClick += onClick;  				
+				this.Width = 200;
+				this.Height = 200;
 				InitializeComponent ();
 				
 			}
@@ -24,13 +25,19 @@ namespace schoolHero {
 			}
 			private void InitializeComponent ()
 			{
+				
+				MenuItem item1 = new MenuItem ("File");
+				MenuItem item2 = new MenuItem ("Exit");
+				MenuItem [] items = new MenuItem [] {item1, item2};
+				context_menu = new 	ContextMenu (items);
+
 
 			}
  		void MainFrm_MouseMove(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
             {
-                Point myPosittion = MousePosition;
+			  	Point myPosittion = MousePosition;
                 myPosittion.Offset(-_frmPoint.X, -_frmPoint.Y);
                 Location = myPosittion;
             } 
@@ -38,8 +45,21 @@ namespace schoolHero {
 
         void MainFrm_MouseDown(object sender, MouseEventArgs e)
         {
+			 if (e.Button == MouseButtons.Left)
+            {
             _frmPoint.X = e.X;
             _frmPoint.Y = e.Y;
+			}
         }
+
+		void onClick (Object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Right)
+			{
+			Point pnt;
+			pnt = PointToClient (MousePosition);
+			context_menu.Show (this, pnt);
+			}
+		}
 	}
 }
