dd0f48676047b7bf8a3567143cabf15d726c08b7
ghosttk <ghosttk@hotmail.com>
2019-03-08 12:01:48 +0800

    add openFileDialog

--- addNewGroupForm.cs
+++ addNewGroupForm.cs
@@ -6,17 +6,18 @@ namespace schoolHero {
  public   class addNewGroupForm : Form {
         TextBox tbNames, tbGroup;
 	Label lbNames, lbGroup;
-	Button btnOk;
+	Button btnOk, openFileDialogBTN;
 	Panel panel;
+	
         public addNewGroupForm ()
         {
 	    Width = 640;
 	    Height = 480;
 	    lbNames = new Label();
-	    lbNames.Text = "Names";
+	    lbNames.Text = "姓名";
 		lbNames.Dock = DockStyle.Top;
             tbNames = new TextBox();
-		tbNames.Text = "Names";
+		tbNames.Text = "列表";
             tbNames.Multiline = true;
             tbNames.Dock = DockStyle.Top;
 		tbNames.Height = 300;
@@ -30,9 +31,14 @@ namespace schoolHero {
 		btnOk.Text ="&Ok";
 		btnOk.Dock = DockStyle.Top;
 		btnOk.Click += onOk;
+		openFileDialogBTN = new Button();
+		openFileDialogBTN.Text = "打开文件";
+		openFileDialogBTN.Click += openFileDialogBTN_Click;
+
 
 		panel = new Panel();
 		panel.Dock = DockStyle.Fill;
+		panel.Controls.Add(openFileDialogBTN);
             panel.Controls.Add(btnOk);
             panel.Controls.Add(tbNames);
             panel.Controls.Add(lbNames);
@@ -59,6 +65,25 @@ namespace schoolHero {
             //保存创建好的XML文档
             xmldoc.Save("data/"+tbGroup.Text+".xml");
 	  }
+
+        private void openFileDialogBTN_Click(object sender, System.EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";//注意这里写路径时要用c:\\而不是c:\
+            openFileDialog.Filter = "文本文件|*.*|C#文件|*.cs|所有文件|*.*";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.FilterIndex = 1;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+			/*
+                fName = openFileDialog.FileName;
+                File fileOpen = new File(fName);
+                isFileHaveName = true;
+                richTextBox1.Text = fileOpen.ReadFile();
+                richTextBox1.AppendText("");
+		    */
+            }
+        }
     }
 
 }
